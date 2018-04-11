1797c83ef1c495d5a5b88cc234eea50934d64807
Mustafa Ozan Alpay <frozsgy@gmail.com>
2018-04-11 15:10:19 +0300

    Bugfixes regarding Part II & minor improvements
    Changed the way to check if the produced result is correct or not with contributions from a classmate.
    
    Added control to check if the I/O pairs exist and readable (which caused division by zero error if not)

--- CENG140/THE1/Tester/tester.py
+++ CENG140/THE1/Tester/tester.py
@@ -4,30 +4,33 @@ import glob
 tests1=[]
 tests2=[]
 for root, dirs, files in os.walk('tests'):
-    tests1 += glob.glob(os.path.join(root, '1_*.in'))
-    tests2 += glob.glob(os.path.join(root, '2_*.in'))
+	tests1+=glob.glob(os.path.join(root, '1_*.in'))
+	tests2+=glob.glob(os.path.join(root, '2_*.in'))
 def removejunk() :
 	os.system("rm -rf t1p1i t1p1r t1p2")
 	os.system("rm -rf results/")
 p1c=len(tests1)
 p2c=len(tests2)
+if(p1c+p2c == 0) :
+	print "\n\tError: No files found in tests folder.\n Please make sure that you have the I/O pairs in the tests folder, and the folder & the files have proper CHMOD permissions.\n"
+	exit()
 true1=0
 true2=0
 true3=0
 liners="----------------------------------------------------------------------"
 def p1i():
-    global true2
-    global tests1
-    try:
-        removejunk()
-        os.system("mkdir results")
-        os.system("gcc the1_iterative.c -Wall -ansi -pedantic-errors -o t1p1i")
-        print "\tTesting Iterative Solution"
-    except:
-        raise
-        exit()
-    #tests for part 1 iterative
-    for i in xrange(p1c):
+	global true2
+	global tests1
+	try:
+		removejunk()
+		os.system("mkdir results")
+		os.system("gcc the1_iterative.c -Wall -ansi -pedantic-errors -o t1p1i")
+		print "\tTesting Iterative Solution"
+	except:
+		raise
+		exit()
+	#tests for part 1 iterative
+	for i in xrange(p1c):
 		j=str(i+1)
 		start=time.time()
 		command="./t1p1i < "+tests1[i]+" > results/1_i"+j+".out"
@@ -58,19 +61,19 @@ def p1i():
 				print "\t "+om+"\t\t\t "+em
 			print liners
 def p1r():
-    global true1
-    global tests1
+	global true1
+	global tests1
 	#tests for part 1 recursive
-    print liners
-    try:
-        removejunk()
-        os.system("mkdir results")
-        os.system("gcc the1_recursive.c -Wall -ansi -pedantic-errors -o t1p1r")
-        print "\tTesting Recursive Solution"
-    except:
-        raise
-        exit()
-    for i in xrange(p1c):
+	print liners
+	try:
+		removejunk()
+		os.system("mkdir results")
+		os.system("gcc the1_recursive.c -Wall -ansi -pedantic-errors -o t1p1r")
+		print "\tTesting Recursive Solution"
+	except:
+		raise
+		exit()
+	for i in xrange(p1c):
 		j=str(i+1)
 		start=time.time()
 		command="./t1p1r < "+tests1[i]+" > results/1_r"+j+".out"
@@ -101,16 +104,16 @@ def p1r():
 				print "\t "+om+"\t\t\t "+em
 			print liners
 def p2():
-    global true3
-    global tests2
-    try:
-        removejunk()
-        os.system("mkdir results")
-        os.system("gcc the1_part2.c -Wall -ansi -pedantic-errors -o t1p2")
-    except:
-        raise
-        exit()
-    for i in xrange(p2c):
+	global true3
+	global tests2
+	try:
+		removejunk()
+		os.system("mkdir results")
+		os.system("gcc the1_part2.c -Wall -ansi -pedantic-errors -o t1p2")
+	except:
+		raise
+		exit()
+	for i in xrange(p2c):
 		j=str(i+1)
 		start=time.time()
 		command="./t1p2 < "+tests2[i]+" > results/2_"+j+".out"
@@ -121,7 +124,16 @@ def p2():
 		ees=expected.split("\n")
 		oes=outs.split("\n")
 		print liners
-		if(ees == oes):
+		inp=open(tests2[i],"r").read()
+		# bugfix by yorum uyanik
+		inp2=inp.split("\n")
+		total=calc(inp2,oes,0,1,0,0)
+		ec=ees[0].index(',')
+		oc=oes[0].index(',')
+		energyofexpected=ees[0][ec:]
+		energyofout=oes[0][oc:]
+		if(energyofexpected == energyofout and total==int(ees[0][ec+16:]) and ees[1]==oes[1]):
+			# end of bugfix by yorum uyanik
 			print "\tTest %s passed!" % tests2[i][6:-3]
 			true3+=1
 		else :
@@ -140,6 +152,16 @@ def p2():
 					om=""
 				print "\t "+om+"\t\t\t "+em
 			print liners
+# bugfix by yorum uyanik
+def calc(inp,out,i,j,total,move):
+	total=total+int(inp[j][2*i])*40
+	if(out[0][2*(move)]=="S"):
+		return calc(inp,out,i,j+1,total,move+1)
+	elif(out[0][2*(move)]=="E"):
+		return calc(inp,out,i+1,j,total,move+1)
+	else:
+		return total
+# end of bugfix by yorum uyanik
 def getinput():
 	global tests
 	try:
@@ -150,7 +172,7 @@ def getinput():
 	except:
 		print "Please enter a valid input"
 		getinput()
-print "Tester for 2017-2018 CENG140 Take Home Exam 1"
+print "\nTester for 2017-2018 CENG140 Take Home Exam 1"
 print liners
 print "To add new tests to the tester, just copy and paste the in and out files to the tests directory."
 print "Remember: I/O pairs for Part I  should have the following name format: 1_name.in & 1_name.out"
@@ -174,6 +196,7 @@ else :
 	p1i()
 	print liners
 	p1r()
+	print liners
 	print liners
 	print "\tTests for Part II"
 	p2()
